1c80f16d1c9f9655c772e170df1c5b139bd4e903
majaliju <majalijunyc@gmail.com>
2022-12-01 19:50:18 -0500

    imported the routes from project4 heroku version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
+  #& Defines the root path route ("/")
+  #& root "articles#index"
+
+  ##~ FOR THE ARTIST-CONCERTS-VENUES DISPLAYS
+  #& get all the artists-genres-venues-concerts-posts; to save to each state
+  get '/artists', to: "artists#index"
+  get '/artists/:id', to: "artists#show"
+  get '/genres', to: "genres#index"
+  get '/venues', to: "venues#index"
+  get '/concerts', to: "concerts#index"
+  get '/posts', to: "posts#index"
+
+  # #& get info about the artist's upcoming shows
+  # get '/upcoming_shows', to: "artists#upcoming_shows"
+
+
+  ##~ FOR THE POSTS CREATION/EDITS/DELETION
+  post '/new_post', to: "posts#create"
+
+  ##~ THE LOGIN/LOGOUT ROUTES
+  #& to create a new user outright
+  post "/new_user", to: "users#create"
+  #& to login our user
+  post "/login", to: "sessions#create"
+  #& to keep the user logged in
+  get "/me", to: "users#show"
+  #& to log the user out
+  delete "/logout", to: "sessions#destroy"
+
+  #& show all the users
+  get "/users", to: "users#index"
+
+  ##~ SESSION & COOKIES INFO
+  #& shows session_id and sessions info
+  get "/show_session", to: "application#show_session"
+  #& displays cookies
+  get "/cookies", to: "application#show_cookies"
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
