39376060f12bb63c9a3727fa410945a1ceaacd0c
majaliju <majalijunyc@gmail.com>
2022-12-15 19:12:38 -0500

    how_many_tickets is validated where h_m_t must be more than 0

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,10 @@ class Post < ApplicationRecord
 
   validates :for_sale, presence: :true
   validates :body, presence: true, length: { in: 5..250 }
-  validates :how_many_tickets, presence: true, numericality: { only_integer: true }
+  validates :how_many_tickets, presence: true, numericality: { greater_than: 0 }
   validates :concert_id, presence: true
   validates :user_id, presence: true
-    ## make sure how_many_tickets is not less than 1 
 
-  ## gotta check about writing validation here 
 
 
 
