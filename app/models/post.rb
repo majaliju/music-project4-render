7a3c4e2894ede1f5e8bf4e75aead19a182a83c31
majaliju <majalijunyc@gmail.com>
2022-12-15 19:16:23 -0500

    added notes on validating post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :concert
 
-  validates :for_sale, presence: :true
+  ## validate only for boolean values
+  validates :for_sale, presence: :true 
   validates :body, presence: true, length: { in: 5..250 }
   validates :how_many_tickets, presence: true, numericality: { greater_than: 0 }
+
+  ## look into the validating assocations
   validates :concert_id, presence: true
   validates :user_id, presence: true
 
