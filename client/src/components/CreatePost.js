e7dffa8150d9652475059e575d088f7c405d80e4
majaliju <majalijunyc@gmail.com>
2022-12-18 17:22:15 -0500

    cleaned up CreatePost with comments & deletions

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -2,20 +2,20 @@ import { useState, useEffect } from 'react';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 
 function CreatePost({ user, concerts, setPosts, posts }) {
-  const navigate = useNavigate();
   const [body, setBody] = useState('');
   const [ticketAmount, setTicketAmount] = useState(0);
   const [error, setError] = useState([]);
   const [success, setSuccess] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
+  const navigate = useNavigate();
   const location = useLocation();
   let isSelling = location.state.isSelling;
   let concertID = location.state.concertID;
-  let artistID = location.state.artistID;
 
   //* updates for IndividualPost are being acted on artists state (thisArtist.post)
 
+  //* resetting our states when a new page renders
   useEffect(() => {
     setSuccess('');
     setError([]);
@@ -57,7 +57,6 @@ function CreatePost({ user, concerts, setPosts, posts }) {
     });
   };
 
-  //! disable submit button after successful submission
   return (
     <div>
       <div class='px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8'>
@@ -153,34 +152,6 @@ function CreatePost({ user, concerts, setPosts, posts }) {
             </button>
           </form>
         </div>
-        {/* {error.map((each) => {
-              console.log('each : ', each);
-              <h1 class='text-2xl font-bold text-center text-indigo-600 sm:text-3xl'>
-                {each}
-              </h1>;
-            })} */}
-
-        {/* {error !== []
-          ? error.map((each) => {
-              <div class='alert alert-warning shadow-lg'>
-                <div>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    class='stroke-current flex-shrink-0 h-6 w-6'
-                    fill='none'
-                    viewBox='0 0 24 24'>
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
-                    />
-                  </svg>
-                  <span>{each}</span>
-                </div>
-              </div>;
-            })
-          : null} */}
       </div>
     </div>
   );
