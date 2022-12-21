8682e04d4b805f32ae6b84053f2b3d85b0cd4035
majaliju <majalijunyc@gmail.com>
2022-12-20 20:56:36 -0500

    test

--- db/migrate/20221221014905_create_posts.rb
+++ db/migrate/20221221014905_create_posts.rb
@@ -2,10 +2,10 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :comment_body
-      t.boolean :for_sale
       t.integer :tickets
-      t.integer :concert_id
+      t.boolean :for_sale
       t.integer :user_id
+      t.integer :concert_id
 
       t.timestamps
     end
